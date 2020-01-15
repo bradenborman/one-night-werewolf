82c46c9990d72db3c657ca7e690a3aea3accb6a1
Brady Borman <bradenborman@hotmail.com>
2020-01-15 15:38:39 -0600

    .

---
+++ src/main/java/borman/onenight/OnenightApplication.java
@@ -0,0 +1,13 @@
+package borman.onenight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnenightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnenightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/borman/onenight/OnenightApplicationTests.java
@@ -0,0 +1,13 @@
+package borman.onenight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnenightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
