df7f8075bd0f0f5497cf7287bfe9ff1caf094532
Brady Borman <bradenborman@hotmail.com>
2020-01-15 20:48:13 -0600

    Join game works too

--- src/main/java/borman/onenight/controllers/websocket/GameSetupController.java
+++ src/main/java/borman/onenight/controllers/websocket/GameSetupController.java
@@ -1,19 +1,16 @@
 package borman.onenight.controllers.websocket;
 
 import borman.onenight.data.PlayingList;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 @Controller
 public class GameSetupController {
-
 
     @MessageMapping("/join-game")
     @SendTo("/one-night/users-playing")
@@ -23,6 +20,5 @@ public class GameSetupController {
         allPlaying.add(input);
         return allPlaying;
     }
-
 
 }
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,8 +1,13 @@
 function startGame() {
-
     var username = prompt("Please enter your name: ");
     var lobbyNumber = "";
      $.post("/startNewLobby", function(data, status){
            window.location.href = "/lobby/" + data + "/?playerName=" + username
       });
+}
+
+function joinGame() {
+    var lobbyNumber = prompt("Please enter ID of lobby: ");
+    var username = prompt("Please enter your name: ");
+    window.location.href = "/lobby/" + lobbyNumber + "/?playerName=" + username
 }
